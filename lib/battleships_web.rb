dfa5fdea3ba8fcf2ad985228374ae78f30488029
Sivan Patel <sivan.patel@googlemail.com>
2015-08-04 10:47:45 +0100

    Added one cucumber test for name

---
+++ lib/battleships_web.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+
+class BattleshipsWeb < Sinatra::Base
+
+  set :views, Proc.new { File.join(root, "..", "views") }
+
+  get '/' do
+    'Hello BattleshipsWeb!'
+    erb :index
+  end
+
+  get '/game' do
+    erb :game
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
